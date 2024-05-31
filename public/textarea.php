8540aeb3d8eb9a8e4eafba156d3032cf76b44595
NOM513 <kd1358629@st.kobedenshi.ac.jp>
2024-05-31 14:23:10 +0900

    text

--- public/textarea.php
+++ public/textarea.php
@@ -5,15 +5,12 @@
     <title>Text Input Result</title>
 </head>
 <body>
-    <p>以下の1と2は、それぞれ別々にpタグでくくること</p>
     <p>入力された文章はつぎのとおりです。</p>
     <?php
     if (isset($_POST['submit'])) {
         $input_text = htmlspecialchars($_POST['input_text'], ENT_QUOTES, 'UTF-8');
         echo "<p>{$input_text}</p>";
-    } else {
-        echo "<p>フォームが送信されていません。</p>";
-    }
+    } 
     ?>
     <br>
     <button onclick="window.history.back();">戻る</button>
